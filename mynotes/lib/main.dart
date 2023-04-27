6195e2437174930c54e74b24f3b0c6cf63e08956
danielatego <danielatego75@gmail.com>
2023-04-27 08:26:53 +0300

    to prevent whole code reexecution on hot reload

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    ),);
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
