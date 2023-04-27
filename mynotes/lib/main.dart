71d88858c16f1a9261074cbe4cc2219499670e4c
danielatego <danielatego75@gmail.com>
2023-04-27 09:57:44 +0300

    init state and dispose+controllers to textfields

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -22,14 +22,30 @@ class _HomePageState extends State<HomePage> {
   late final TextEditingController _password;
 
   @override
+  void initState() {
+    _email=TextEditingController ();
+     _password=TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Register'),
       ),
       body: Column(
         children: [
-          TextField(),
-          TextField(),
+          TextField(
+            controller:_email,            
+          ),
+          TextField(
+            controller:_password,
+          ),
           TextButton(
             onPressed: () async {
 
