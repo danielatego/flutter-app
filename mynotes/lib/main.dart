b1124088bde4c7f4b2b1512b822ad32497e90394
danielatego <danielatego75@gmail.com>
2023-04-27 09:22:25 +0300

    centering a button in the body

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -17,6 +17,12 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Register'),
       ),
+      body: Center(
+        child: TextButton(
+          onPressed: (){},
+          child: const Text('Register'),
+          ),
+      )
     );
   }
 }
