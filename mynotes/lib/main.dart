d09af260d286aacd5346182b8bd5aa65ab515daf
danielatego <danielatego75@gmail.com>
2023-04-30 07:40:50 +0300

    register view function

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -9,9 +9,24 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const RegisterView(),
     ),);
 }
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 class HomePage extends StatefulWidget {
 
   const HomePage({super.key});
