fd33297bc3a76a11c4fcd232af5fd7c5bc14db8a
danielatego <danielatego75@gmail.com>
2023-04-30 08:20:38 +0300

    firebase auth  for login view

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/views/login_view.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 void main() {
@@ -9,7 +10,7 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegisterView(),
+      home: const LoginView(),
       ),
     );
 }
