80981e6a4a42c4d27f0b5c09efc2b3c27fc9ec8c
danielatego <danielatego75@gmail.com>
2023-05-03 08:35:20 +0300

    email verification

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/firebase_options.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -7,7 +9,7 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
       ),
     );
 }
@@ -17,6 +19,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home'),
+      ),
+      body: FutureBuilder(
+        future:  Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+          ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+            final user =FirebaseAuth.instance.currentUser;
+            //final emailVerified= user?.emailVerified ?? false;
+            if (user?.emailVerified ?? false){
+              print('You are a verified user');
+            }else{
+              print('You need to verify your email first');
+            }
+            return const Text('Done');
+            default:
+            return const Text('Loading...');
+              }
+        },
+      )
+    );
+  }}
