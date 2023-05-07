21ece58d5e2917f2a72b7645f3e092af7281ebca
danielatego <danielatego75@gmail.com>
2023-05-07 08:27:27 +0300

    changing routes to constants #avoidHardcoding

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'views/login_view.dart';
 import 'views/register_view.dart';
@@ -13,10 +14,10 @@ import 'views/veriry_email_view.dart';void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/':(context)=> const LoginView(),
-        '/register/':(context)=> const RegisterView(),
-        '/verify/':(context) => const VerifyEmailView(),
-        '/notes/':(context) => const NotesView(),
+        loginRoute:(context)=> const LoginView(),
+        registerRoute:(context)=> const RegisterView(),
+        verifyRoute:(context) => const VerifyEmailView(),
+        notesRoute:(context) => const NotesView(),
       },
       ),
     );
@@ -78,7 +79,7 @@ class _NotesViewState extends State<NotesView> {
                 if (shouldLogout){
                   await FirebaseAuth.instance.signOut();
                   Navigator.of(context).pushNamedAndRemoveUntil(
-                    '/login/', 
+                    loginRoute, 
                     (_) => false,
                     );
                 }
