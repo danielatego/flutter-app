1428d2944e29d84e2d6c18499c39081904d1d232
danielatego <danielatego75@gmail.com>
2023-07-05 23:34:36 +0300

    generic error dialog

--- two_a/lib/views/login_view.dart
+++ two_a/lib/views/login_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:two_a/firebase/authentication/exceptions.dart';
+import 'package:two_a/firebase/bloc/auth_bloc.dart';
+import 'package:two_a/firebase/bloc/auth_state.dart';
+
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,15 +13,33 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState(){
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    _email.dispose();
+    _password.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        body: const Center(
-          child: Text('login view'),
-        ),
-      ),
-    );
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async{
+        if (state is AuthStateLoggedOut){
+          if(state.exception is UserNotFoundAuthException || state.exception is WrongDetailsAuthException){
+            await 
+          }
+        }
+      },
+      child: Container(),
+    )
   }
 }
